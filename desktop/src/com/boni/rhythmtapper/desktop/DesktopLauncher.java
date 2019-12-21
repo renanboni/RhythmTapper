31bf6911b2e49efe435b64e0f5e97de6e51977f0
renan.boni <renan.boni@usp.br>
2019-12-20 23:05:20 -0300

    Added missing library

--- desktop/src/com/boni/rhythmtapper/desktop/DesktopLauncher.java
+++ desktop/src/com/boni/rhythmtapper/desktop/DesktopLauncher.java
@@ -2,11 +2,10 @@ package com.boni.rhythmtapper.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.boni.rhythmtapper.RhythmTapper;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new RhythmTapper(), config);
+		// new LwjglApplication(new RhythmTapper(), config);
 	}
 }
